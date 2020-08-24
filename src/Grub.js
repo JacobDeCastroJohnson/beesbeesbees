30a7b226fa4ce4c3a0fac1d23c2d2c01e49ac1a4
JacobDeCastroJohnson <Jacob.DeCastro.J@gmail.com>
2020-08-24 11:13:38 -0700

    Complete Pseudoclassical Instanciation of Grub Class

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,15 @@
 var Grub = function() {
+  //vard this = Object.create(Grub.prototype)
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
+
+  //retunr this;
 };
+
+Grub.prototype.eat = function() {
+
+}
+
+
 
