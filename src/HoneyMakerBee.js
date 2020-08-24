7a726dd5a70bd9d0e6c38feb40518c01b9bd7159
JacobDeCastroJohnson <Jacob.DeCastro.J@gmail.com>
2020-08-24 12:31:27 -0700

    Complete Pseudoclassical Instanciation of HoneyMakerBee class

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,24 @@
 var HoneyMakerBee = function() {
+
+  //Call the Bee class
+  Bee.call(this);
+
+  this.age = 10;
+  this.job = 'make honey';
+  // this.color; --> inherited from bee
+  // this.food; --> inherited from bee
+  this.honeyPot = 0;
 };
+
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+}
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+}
+
 
