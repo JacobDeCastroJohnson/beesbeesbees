e7e6c57ffbe72fdb202768ef53e5c764a4a08106
JacobDeCastroJohnson <Jacob.DeCastro.J@gmail.com>
2020-08-24 12:51:31 -0700

    Complete Pseudoclassical Instanciation of RetiredForagerBee class

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,35 @@
 var RetiredForagerBee = function() {
+
+  //Inherit from the ForagerBee Superclass
+  ForagerBee.call(this);
+
+  //Overwrite methods from superclass
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
+
+  //Inherited properties
+  //this.food = 'jelly';
+  //this.eat method
+  //this.treasureChest = [];
+
+
+};
+
+//Set the prototype
+RetiredForagerBee.prototype = Object.create(Grub.prototype);
+
+//Delegate constructor pointer
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
+//forage method
+RetiredForagerBee.prototype.forage = function () {
+  return 'I am too old, let me play cards instead';
+};
+
+//gamble method
+RetiredForagerBee.prototype.gamble = function (treasure) {
+  this.treasureChest.push(treasure);
 };
 
